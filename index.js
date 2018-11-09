075aef0c31d44c9ab806e4f7eb5719935c796a04
Alon Niv <oakfang@gmail.com>
2018-11-09 21:15:00 +0200

    gitpkg

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = async function({ body: data }) {
+  this.log.info(`Current sample from: ${data.ts}`);
+  if (!(await this.state.get())) {
+    await this.state.set({});
+  }
+  const state = await this.state.get();
+  if (!state[data.card.type]) {
+    state[data.card.type] = [];
+  }
+  state[data.card.type].push(data.user);
+  await this.state.set(state);
+  Object.keys(state).forEach(ctype => {
+    this.log.info(`${ctype}: ${state[ctype].length}`);
+  });
+};
